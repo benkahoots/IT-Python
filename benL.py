700a68e0ed855d40bc80169afb88efd0c02046a7
benkahoots <benkahoots101@gmail.com>
2015-02-09 10:50:04 +1100

    Create benL.py

---
+++ benL.py
@@ -0,0 +1,54 @@
+
+print "Hello. I am the Python Calculator! What is your name?"
+
+def name = raw_input("Hello. I am the Python Calculator! What is your name?")
+
+def emotion = raw_input("Well, " +name+", I am the Python Calculator. How is your day going?")
+
+if emotion = "Good":
+	print "That's fantastic. I'm sure I will make your day even better!"
+
+elif emotion = "Bad":
+	print "That's a shame. If it helps, I'm having a good day!"
+
+	
+fnum = raw_input("Well, this is a calculator, so could you please put in your first number.")  
+operator = raw_input("Now, what would you like to do with it?")
+snum = raw_input(And your second number?)
+
+
+def Add(fnum, snum)
+	return fnum + snum
+	
+def Sub(fnum, snum)
+	return fnum - snum
+	
+def Mul(fnum, snum)
+	return fnum * snum
+	
+def Pow(fnum, snum)
+	return fnum ** snum
+	
+def Div(fnum, snum)
+	return fnum / snum
+	
+while operator != "Q":
+	if operator == "+"
+	ans = Add(fnum, snum)
+	print ans 
+
+	elif operator == "-"
+	ans = Sub(fnum, snum)
+	print ans 
+	
+	elif operator == "*"
+	ans = Mul(fnum, snum)
+	print ans 
+	
+	elif operator == "**"
+	ans = Pow(fnum, snum)
+	print ans
+	
+	elif operator == "/"
+	ans = Add(fnum, snum)
+	print ans 
